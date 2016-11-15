0027bd90c7be256ff0d7d59ab8a513a9a4ae12cf
bluedragonfly <noki.dev@gmail.com>
2016-11-15 16:48:52 +0100

    Uodate User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base #User(Model) Inherit from ActiveRecord::Base it's a basic model class
 
-  has_many :accounts #Means that there is many acounts in a bank (One to many rel)
-  validates :name, presence: true # Used tko allow creation of an item they are required arguments
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :mail, presence: true
+  validates :first_name, presence:true
+  validates :last_name, presence:true
+  validates :age, presence:true
+  # Used tko allow creation of an item they are required arguments
   #todo loook in docs for more vars
 
   def todo
